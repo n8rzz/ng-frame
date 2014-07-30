d428f4c8fada41c79e869577893349e0253959ff
n8 <teamtomkins23@gmail.com>
2014-07-30 18:46:45 -0500

    commented out comments

--- single.php
+++ single.php
@@ -125,11 +125,11 @@
 <?php /*<!--	</div> /.row -->*/ ?>
 </div><?php /*<!-- /.container-fluid -->*/ ?>
 
-		<?php
+		<?php /*
             // If comments are open or we have at least one comment, load up the comment template
             if ( comments_open() || '0' != get_comments_number() )
                 comments_template( '', true );
-        ?>
+        */ ?>
 		
 		<?php endwhile; ?>
 	<?php else : ?>
